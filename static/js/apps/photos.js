f76548bd05adaa683c67047e1a67aeb5e7b0177e
Adam Lord <adam.lord@gmail.com>
2016-09-21 11:33:56 -0700

    Update instagram feed api creds

--- static/js/apps/photos.js
+++ static/js/apps/photos.js
@@ -3,40 +3,40 @@ define([
     'bootstrap',
     'magnific-popup',
     'instafeed',
-],
+  ],
 
-function ($) {
-    $(function(){
-        var feed = new Instafeed({
-            clientId: 'b1b89352013d4bc58352ff847c69c3c3',
-            get: 'user',
-            userId: 201585387,
-            accessToken: '201585387.b1b8935.09b707305b2b4cf3a7e68aab25a66dff',
-            resolution: 'standard_resolution',
-            template: '<a href="{{image}}" class="thumb" title="{{caption}}"><img src="{{image}}"" /></a>',
-            after: function(){
-                $('#instafeed_spinner').fadeOut('fast');
-            }
-        });
-        feed.run();
+  function($) {
+    $(function() {
+      var feed = new Instafeed({
+        clientId: 'e57e58115b304e319ef65ad52d8555ba',
+        get: 'user',
+        userId: 201585387,
+        accessToken: '201585387.e57e581.dafbe9ceedf4475b909749652bbdb290',
+        resolution: 'standard_resolution',
+        template: '<a href="{{image}}" class="thumb" title="{{caption}}"><img src="{{image}}"" /></a>',
+        after: function() {
+          $('#instafeed_spinner').fadeOut('fast');
+        }
+      });
+      feed.run();
 
-		$('.album-popup').magnificPopup({
-			delegate: 'a',
-			type:'image',
-			gallery: {
-				enabled: true
-			},
-			zoom: {
-				enabled: true,
-				duration: 300,
-				easing: 'ease-in-out',
-				opener: function(openerElement) {
-					return openerElement.is('img') ? openerElement : openerElement.find('img');
-				}
-			},
-            image: {
-                titleSrc: 'title'
-            }
-		});
+      $('.album-popup').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+          enabled: true
+        },
+        zoom: {
+          enabled: true,
+          duration: 300,
+          easing: 'ease-in-out',
+          opener: function(openerElement) {
+            return openerElement.is('img') ? openerElement : openerElement.find('img');
+          }
+        },
+        image: {
+          titleSrc: 'title'
+        }
+      });
     });
-});
+  });
