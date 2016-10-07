3548a4e2dc277d61c645c301d564f6906a4f4c22
Adam Lord <adam.lord@gmail.com>
2016-10-07 09:59:01 -0700

    Hide commission check on load, reformat js, open dbadmin in new tab

--- static/js/apps/fidouche.js
+++ static/js/apps/fidouche.js
@@ -1,7 +1,7 @@
-$(function ($) {
+$(function($) {
 	// Methods
-	function updateFields(){
-		if($('#auto_calc').bootstrapSwitch('state') === true){
+	function updateFields() {
+		if ($('#auto_calc').bootstrapSwitch('state') === true) {
 			var commissionField = $('#id_commission_percentage');
 			var gross = $('#id_gross');
 			var fee = $('#id_fee');
@@ -16,11 +16,11 @@ $(function ($) {
 			var tourExpense = $('#tour_expense_share');
 			var production_cost = 0;
 			var iem_cost = 0;
-			$('.production-cost').each(function(i,n){
+			$('.production-cost').each(function(i, n) {
 				var amt = parseFloat($(n).val() || 0);
-				if($('#id_production_payment-' + i +'-category').val() == FL.VARS.iem_cat){
+				if ($('#id_production_payment-' + i + '-category').val() == FL.VARS.iem_cat) {
 					iem_cost += amt;
-				}else {
+				} else {
 					production_cost += amt;
 				}
 			});
@@ -37,64 +37,77 @@ $(function ($) {
 			// receiveable
 			var c, n, acc, mp = '';
 			var cb;
-			if(g>0){
+			if (g > 0) {
 				if (fee.val() > 0) {
 					// base commission off fee
 					cb = fee.val();
 				} else {
 					cb = g;
 				}
-				c = (parseFloat((cb - pc) * (cp/100)) || 0).toFixed(2);
+				c = (parseFloat((cb - pc) * (cp / 100)) || 0).toFixed(2);
 				n = (parseFloat(g - c - (pc + iem + ps + a + o + t)) || 0).toFixed(2);
 				mp = (parseFloat(n / 14) || 0).toFixed(2);
-				acc = (parseFloat(n - (p * 14)) ||0).toFixed(2);
+				acc = (parseFloat(n - (p * 14)) || 0).toFixed(2);
 			}
 			commission.val(c).change();
 			max.html(mp).parent().fadeIn('fast');
 			net.val(n).change();
 			account.val(acc).change();
+			if ($('#id_commission_withheld').is(':checked')) {
+				$('#commission_check').fadeOut('fast');
+			}
 		}
 	}
 	var _updateFields = _.throttle(updateFields, 500);
 
-	function processItemized(){
+	function processItemized() {
 		var printCosts = 0;
 		var shipCosts = 0;
 		var adCosts = 0;
 		var otherCosts = 0;
-		$('#expenses_formset tbody tr').each(function(){
+		$('#expenses_formset tbody tr').each(function() {
 			var thisCat = $(this).find('.category option:selected').val();
 			var thisAmount = parseFloat($(this).find('.expense-amount input').val()) || 0;
-			switch(thisCat){
+			switch (thisCat) {
 				case 'print':
-				printCosts += thisAmount;
-				break;
+					printCosts += thisAmount;
+					break;
 				case 'ship':
-				shipCosts += thisAmount;
-				break;
+					shipCosts += thisAmount;
+					break;
 				case 'ads':
-				adCosts += thisAmount;
-				break;
+					adCosts += thisAmount;
+					break;
 				default:
-				otherCosts += thisAmount;
-				return;
+					otherCosts += thisAmount;
+					return;
 			}
 		});
-		if(printCosts + shipCosts !== 0){$('#id_print_ship_cost').val(printCosts + shipCosts).change();}
-		if(adCosts !== 0){$('#id_ads_cost').val(adCosts).change();}
-		if(otherCosts !== 0){$('#id_other_cost').val(otherCosts.toFixed(2)).change();}
+		if (printCosts + shipCosts !== 0) {
+			$('#id_print_ship_cost').val(printCosts + shipCosts).change();
+		}
+		if (adCosts !== 0) {
+			$('#id_ads_cost').val(adCosts).change();
+		}
+		if (otherCosts !== 0) {
+			$('#id_other_cost').val(otherCosts.toFixed(2)).change();
+		}
 		_updateFields();
 	}
+
 	function cloneMore(selector, type) {
 		var newElement = $(selector).clone(true);
 		var total = $('#id_' + type + '-TOTAL_FORMS').val();
 		newElement.find(':input').each(function() {
-			var name = $(this).attr('name').replace('-' + (total-1) + '-','-' + total + '-');
+			var name = $(this).attr('name').replace('-' + (total - 1) + '-', '-' + total + '-');
 			var id = 'id_' + name;
-			$(this).attr({'name': name, 'id': id}).val('').removeAttr('checked');
+			$(this).attr({
+				'name': name,
+				'id': id
+			}).val('').removeAttr('checked');
 		});
 		newElement.find('label').each(function() {
-			var newFor = $(this).attr('for').replace('-' + (total-1) + '-','-' + total + '-');
+			var newFor = $(this).attr('for').replace('-' + (total - 1) + '-', '-' + total + '-');
 			$(this).attr('for', newFor);
 		});
 		total++;
@@ -103,46 +116,46 @@ $(function ($) {
 	}
 
 	// Handlers //
-	$('.factor').on('blur', 'input, select', function(){
+	$('.factor').on('blur', 'input, select', function() {
 		_updateFields();
 	});
-	$('#commission_withheld').on('change', '#id_commission_withheld', function(){
-		if($(this).is(':checked')){
+	$('#commission_withheld').on('change', '#id_commission_withheld', function() {
+		if ($(this).is(':checked')) {
 			$('#commission_check').fadeOut('fast');
-		}else {
+		} else {
 			$('#commission_check').fadeIn('fast');
 		}
 	});
-	$('.payment-method').on('change', '#id_gross_method', function(){
-		if($(this).find('option:selected').val() == 'check'){
+	$('.payment-method').on('change', '#id_gross_method', function() {
+		if ($(this).find('option:selected').val() == 'check') {
 			$('#payment_check_no').fadeIn('fast');
-		}else {
+		} else {
 			$('#payment_check_no').fadeOut('fast');
 		}
 	});
-	$('.set-payout').on('blur', '#id_payout', function(){
+	$('.set-payout').on('blur', '#id_payout', function() {
 		$('#payment_formset .amount input').val($(this).val()).change();
 	});
-	$('.warn').on('change', 'input', function(){
+	$('.warn').on('change', 'input', function() {
 		var parentGroup = $(this).parents('.form-group');
 		parentGroup.removeClass('has-error');
-		if($(this).val() && parseFloat($(this).val()) < 0){
+		if ($(this).val() && parseFloat($(this).val()) < 0) {
 			parentGroup.addClass('has-error');
 		}
 	});
-	$('#itemize_toggle').click(function(){
-		if($(this).hasClass('active')){
+	$('#itemize_toggle').click(function() {
+		if ($(this).hasClass('active')) {
 			$('#id_costs_itemized').val('False').change();
 			$('.expenses-summed input').removeAttr('readonly');
 		} else {
 			$('#id_costs_itemized').val('True').change();
-			$('.expenses-summed input').attr('readonly','readonly');
+			$('.expenses-summed input').attr('readonly', 'readonly');
 		}
 		$('#itemize').collapse('toggle');
 		$(this).blur();
 	});
-	$('#subs_toggle').click(function(){
-		if($(this).hasClass('active')){
+	$('#subs_toggle').click(function() {
+		if ($(this).hasClass('active')) {
 			$('#id_subs').val('False').change();
 		} else {
 			$('#id_subs').val('True').change();
@@ -150,8 +163,8 @@ $(function ($) {
 		$('#sub_payment').collapse('toggle');
 		$(this).blur();
 	});
-	$('#buyouts_toggle').click(function(){
-		if($(this).hasClass('active')){
+	$('#buyouts_toggle').click(function() {
+		if ($(this).hasClass('active')) {
 			$('#id_gross_itemized').val('False').change();
 		} else {
 			$('#id_gross_itemized').val('True').change();
@@ -159,43 +172,43 @@ $(function ($) {
 		$('#buyouts').collapse('toggle');
 		$(this).blur();
 	});
-	$('#expenses_formset').on('blur', '.expense-amount input, .category select', function(){
+	$('#expenses_formset').on('blur', '.expense-amount input, .category select', function() {
 		processItemized();
 	});
-	$('#add_expense_rows').click(function(){
+	$('#add_expense_rows').click(function() {
 		cloneMore('#expenses_formset tr:last', 'expense');
 	});
-	$('#add_production_payments').click(function(){
+	$('#add_production_payments').click(function() {
 		cloneMore('#gig_production tr:last', 'production_payment');
 	});
-	$('#payment_check_all').change(function(){
-		if($(this).prop('checked')){
+	$('#payment_check_all').change(function() {
+		if ($(this).prop('checked')) {
 			$('.paid input').prop('checked', true);
 		} else {
 			$('.paid input').prop('checked', false);
 		}
 	});
 	// On load //
-	if($('#id_costs_itemized').val() == 'True'){
+	if ($('#id_costs_itemized').val() == 'True') {
 		$('#itemize_toggle').click();
 	}
-	if($('#id_subs').val() == 'True'){
+	if ($('#id_subs').val() == 'True') {
 		$('#subs_toggle').click();
 	}
-	if($('#id_gross_itemized').val() == 'True'){
+	if ($('#id_gross_itemized').val() == 'True') {
 		$('#buyouts_toggle').click();
 	}
 	var autoCalcState = Cookies.get('autoCalc') === 'false' ? false : true;
 	$('#auto_calc').attr('checked', autoCalcState);
 	$('#auto_calc').bootstrapSwitch({
-		onSwitchChange: function(event, state){
+		onSwitchChange: function(event, state) {
 			Cookies.set('autoCalc', state);
-			if(state){
+			if (state) {
 				_updateFields();
 			}
 		},
 	}).trigger('change');
-	$(document).ready(function(){
+	$(document).ready(function() {
 		_updateFields();
 	});
 });
