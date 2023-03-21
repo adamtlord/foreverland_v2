c69de9f4c14681294372408242363e0ea20978b3
adamtlord <adam.lord@gmail.com>
2023-03-21 11:10:27 -0700

    update letsencrypt script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(foreverland.com  www.foreverland.com)
+domains=(foreverland.com www.foreverland.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="adam.lord@gmail.com" # Adding a valid address is strongly recommended
