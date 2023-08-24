d3b8f9ba26b1e1b2c59c79caf6113dff8cf931fc
adamtlord <adam@evergrow.com>
2023-08-24 16:01:41 -0700

    remove twitter widgets

--- web/static/js/apps/sitewide.js
+++ web/static/js/apps/sitewide.js
@@ -1,27 +1,11 @@
-define([
-    'jquery',
-    'underscore',
-    'bootstrap'
-],
-
-function ($) {
-    $('html').addClass('js-ready');
-    $('#navbar_toggle').click(function(e){
-        e.preventDefault();
-        $('html').toggleClass('js-nav');
-    });
-    $('#nav_close_btn').click(function(e){
-        e.preventDefault();
-        $('html').removeClass('js-nav');
-    });
-    ! function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-            p = /^http:/.test(d.location) ? 'http' : 'https';
-        if (!d.getElementById(id)) {
-            js = d.createElement(s);
-            js.id = id;
-            js.src = p + "://platform.twitter.com/widgets.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }
-    }(document, "script", "twitter-wjs");
+define(["jquery", "underscore", "bootstrap"], function ($) {
+  $("html").addClass("js-ready");
+  $("#navbar_toggle").click(function (e) {
+    e.preventDefault();
+    $("html").toggleClass("js-nav");
+  });
+  $("#nav_close_btn").click(function (e) {
+    e.preventDefault();
+    $("html").removeClass("js-nav");
+  });
 });
